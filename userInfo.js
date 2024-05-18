055209a5bc760ddaa1f295862b82d28ad03cee9a
Kingsford Ofori <kofori023@st.ug.edu.gh>
2024-05-18 07:54:54 +0000

    git

---
+++ userInfo.js
@@ -0,0 +1,25 @@
+function createUserProfiles(names, modifiedNames) {
+    let userProfiles = [];
+    let id = 1;
+    for (let i = 0; i < names.length; i++) {
+      let userProfile = {
+        originalName: names[i],
+        modifiedName: modifiedNames[i],
+        id: id++
+      };
+      userProfiles.push(userProfile);
+    }
+    return userProfiles;
+  }
+  
+  // Example usage:
+  let names = ["Janet", "Bismark", "Kingsford", "Doris"];
+  let modifiedNames = ["JANET", "bismark", "KINGSFORD", "doris"];
+  let profiles = createUserProfiles(names, modifiedNames);
+  console.log(profiles);
+  
+
+  
+
+
+

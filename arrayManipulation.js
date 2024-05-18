3e222af7d1cbfd214815b0351ebe6748172808f8
Kingsford Ofori <kofori023@st.ug.edu.gh>
2024-05-18 07:15:00 +0000

    task 1

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,3 +1,7 @@
 function processArray(arr) {
     return arr.map(num => num % 2 === 0 ? num ** 2 : num * 3);
   }
+  // Example usage:
+  let inputArray = [1, 2, 3, 4, 5];
+  let resultArray = processArray(inputArray);
+  console.log(resultArray); // Output: [3, 4, 9, 16, 15]

3bc08da2c6d020173247e709a5cf2319effd4088
Kingsford Ofori <kofori023@st.ug.edu.gh>
2024-05-18 07:22:05 +0000

    task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -5,3 +5,16 @@ function processArray(arr) {
   let inputArray = [1, 2, 3, 4, 5];
   let resultArray = processArray(inputArray);
   console.log(resultArray); // Output: [3, 4, 9, 16, 15]
+
+  // task 2
+  function formatArrayStrings(stringArray, numberArray) {
+    let formattedArray = [];
+    for (let i = 0; i < stringArray.length; i++) {
+      if (numberArray[i] % 2 === 0) {
+        formattedArray.push(stringArray[i].toUpperCase());
+      } else {
+        formattedArray.push(stringArray[i].toLowerCase());
+      }
+    }
+    return formattedArray;
+  }
